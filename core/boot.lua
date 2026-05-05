c536e6979ef1db8ee4af5230772304e8a7848bd9
Heights studios <redbuttonthere@gmail.com>
2026-05-05 20:27:38 +0300

    Update boot.lua

--- core/boot.lua
+++ core/boot.lua
@@ -1,1 +1,13 @@
+local log = require("logger")
 
+term.setCursorPos(1,1)
+term.clear()
+
+local os = "lightOS"
+local ver = "1.0"
+
+while True do
+  print(os .. "v" .. ver)
+  print(os .. "basic cmd")
+  break
+end
