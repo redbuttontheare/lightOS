c32127a2fe83d3bda9e58217c504da293404774f
Heights studios <redbuttonthere@gmail.com>
2026-05-05 20:54:14 +0300

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -8,4 +8,15 @@ print("booting from lightOS disk")
 
 sleep(3)
 
-shell.run("disk/core/boot.lua")
+term.setCursorPos(1,1)
+term.clear()
+
+local os = "lightOS"
+local ver = "1.0"
+
+while True do
+  print(os .. "v" .. ver)
+  print(os .. "basic cmd")
+  print("Hello!")
+  break
+end
