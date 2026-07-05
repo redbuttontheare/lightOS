b144d8c8c66cb5637bc99775b8e3a04570a1f400
Heights studios <redbuttonthere@gmail.com>
2026-07-05 17:51:40 +0300

    Project under GNU GPL v3 or above

--- lightOS/init.lua
+++ lightOS/init.lua
@@ -1,3 +1,15 @@
+-- Copyright (C) 2026 redbuttontheare@gmail.com
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+
 local lapi = dofile("lib/lapi.lua")
 local autoexec = dofile("lightOS/autoexec_runner.lua")
 local autoexec_tbl = dofile("lightOS/autoexec.lua")
