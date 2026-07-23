fdc324aa66b48d4248c92f55446fbacc95e889ff
Heights studios <redbuttonthere@gmail.com>
2026-07-23 21:45:10 +0300

    FIX 12.2.1 -> 12.2.2

--- lib/gelaxy/window.lua
+++ lib/gelaxy/window.lua
@@ -1,4 +1,4 @@
-ґ-- Copyright (C) 2026 redbuttontheare@gmail.com
+-- Copyright (C) 2026 redbuttontheare@gmail.com
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
