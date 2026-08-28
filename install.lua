3978e4e0835c827ff94e4e37a004b846211a2834
Heights studios <redbuttonthere@gmail.com>
2026-08-28 11:34:11 +0300

    HatFix

--- install.lua
+++ install.lua
@@ -340,11 +340,13 @@ get("lib/gelaxy/button.lua", "/tmp/gl/button.lua")
 get("lib/gelaxy/window.lua", "/tmp/gl/window.lua")
 get("lib/gelaxy/checkbox.lua", "/tmp/gl/checkbox.lua")
 get("lib/gelaxy/textbox.lua", "/tmp/gl/textbox.lua")
+
+sleep(4)
+
 local Button = dofile("/tmp/gl/button.lua")
 local Checkbox = dofile("/tmp/gl/checkbox.lua")
 local Textbox = dofile("/tmp/gl/textbox.lua")
 
-sleep(4)
 
 term.setBackgroundColor(baseblue)
 term.setTextColor(colors.white)
